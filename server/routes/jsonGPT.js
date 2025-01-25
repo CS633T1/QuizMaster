288a2e7175bb6ed09b95ab258337611a5bb90667
Thomas Adams <tommyadams5@gmail.com>
2025-01-25 10:30:51 -0800

    Added multiple choice prompt text. Added getting started directions to README

--- server/routes/jsonGPT.js
+++ server/routes/jsonGPT.js
@@ -7,6 +7,24 @@ const llmKey = process.env.OPEN_AI_KEY;
 const jsonGPT = async (prompt) => {
   try {
     const openai = new OpenAI({ apiKey: llmKey });
+    const instructions = `Create a list of 10 multiple choice questions based on the following text. 
+    Each question should have 4 answer choices numbered 1 through 4. 
+    Provide this list as a json array named "questions" in the following format:
+    {
+      "questions": [
+        {
+          "question": "Question text here",
+          "options": [
+            "option 1",
+            "option 2",
+            "option 3",
+            "option 4"
+          ],
+          "answer": 1
+        },
+      ]
+    }
+    TEXT: `;
 
     const completion = await openai.chat.completions.create({
       messages: [
@@ -14,7 +32,7 @@ const jsonGPT = async (prompt) => {
           role: "system",
           content: "You are a helpful assistant designed to output JSON.",
         },
-        { role: "user", content: prompt },
+        { role: "user", content: instructions + prompt },
       ],
       model: "gpt-3.5-turbo-0125",
       response_format: { type: "json_object" },
@@ -24,7 +42,7 @@ const jsonGPT = async (prompt) => {
     return result;
   } catch (error) {
     console.error(error);
-    let json_error = { status: error.status, message: error.message };
+    const json_error = { status: error.status, message: error.message };
     return json_error;
   }
 };
