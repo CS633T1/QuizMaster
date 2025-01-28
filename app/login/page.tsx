aff0b814e0786d847c12584fd9da64b7a3b921f9
Tsz Ching Wong <tsz_wong@datahouse.com>
2025-01-28 02:17:56 -1000

    reimplemented signup/login pages

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
+function Login() {
+  return (
+    <Container>
+      {" "}
+      <Typography variant="h3" align="center">
+        Login
+      </Typography>
+    </Container>
+  );
+}
+
+export default Login;
---
+++ app/signup/page.tsx
@@ -0,0 +1,15 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
+function Signup() {
+  return (
+    <Container>
+      {" "}
+      <Typography variant="h3" align="center">
+        Signup
+      </Typography>
+    </Container>
+  );
+}
+
+export default Signup;
